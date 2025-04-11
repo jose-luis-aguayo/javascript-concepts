b95a05f2c2a9b408bce5ffce2b9d52239e8c20c2
Jose Luis Aguayo <jose-luis@MacBook-Pro-de-Jose.local>
2025-04-11 16:46:59 -0600

    adding intel files

--- test-field.js
+++ test-field.js
@@ -29,15 +29,67 @@ console.log(a)
 
 }
 
-let arr = ['{', '{', '[', '[', '(', '(', ')', ')', ']', ']', '}', '}' ];
+// let arr = ['{', '{', '[', '[', '(', '(', ')', ')', ']', ']', '}', '}' ];
 
 
-let mid = (arr.length / 2) - 1;
+// let mid = (arr.length / 2) - 1;
 
-let lastIndex = arr.length - 1;
+// let lastIndex = arr.length - 1;
 
-for(let i = 0; i <= mid; i ++){
-    console.log(arr[i], arr[lastIndex])
-    lastIndex --;
+// for(let i = 0; i <= mid; i ++){
+//     console.log(arr[i], arr[lastIndex])
+//     lastIndex --;
+// }
+
+
+// [0, 1, 2, 3, 4, 5] length = 6 / 2 = 3
+// [0, 1, 2, 3, 4] length = 5 
+
+// function reverseArray(arr){
+//     let arrLength = arr.length;
+//     let mid = 0;
+
+//     mid = Math.floor((arrLength / 2) - 1);
+
+//     for(let i = 0; i <= mid; i++) {
+//         [arr[i], arr[arrLength - 1]] = [arr[arrLength - 1], arr[i]]
+//         arrLength--;
+//     }
+
+//     return arr;
+
+// }
+
+// console.log(reverseArray([1,2,3,4,5,6]))
+
+
+//return index of unique char otherwise return -1
+function firstUniqueChar(a){
+    let uniqueCharFound = false;
+
+    for(let i = 0; i < a.length; i ++){
+        
+        for(let j = 0; j < a.length; j ++){
+
+            
+            if(i == j) continue;
+
+            if(a[i] == a[j]) break;
+
+            if(j == a.length - 1) {
+                uniqueCharFound = true
+            }
+
+            console.log(`comparision ${a[i]} == ${a[j]} ${uniqueCharFound}`)
+            
+        }
+
+        if(uniqueCharFound) {
+            return i
+        }
+
+    }
+    return -1
 }
 
+console.log(firstUniqueChar('abbac'));
