cffe675f4051ba38aaa0e45b773b0274e9f667db
Jose Luis Aguayo <jose-luis@MacBook-Pro-de-Jose.local>
2025-04-21 00:39:43 -0600

    no description

--- algorithms/binary-search.js
+++ algorithms/binary-search.js
@@ -1,15 +1,15 @@
 function binarySearch(arr, target, start = 0, end = arr.length - 1) {
     let mid = ~~((start + end) / 2); //4
     
-    console.log(`start ${start} end ${end} mid ${mid} array${arr.slice(start, end)}`)
-    
+    //console.log(`start ${start} end ${end} mid ${mid} array${arr.slice(start, end)}`)
+
     if (start > end) {
         return -1;
     }
 
     if (arr[mid] === target) {
         return mid;
-    } else if (arr[mid] < target) {
+    } else if (target > arr[mid]) {
         
         return binarySearch(arr, target, mid + 1, end);
     } else {
@@ -21,12 +21,7 @@ function binarySearch(arr, target, start = 0, end = arr.length - 1) {
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // length 10 
 const target = 2;
 const result = binarySearch(arr, target);
-console.log(result); // Output: 6
+console.log(result); 
 
 
-/**
- * 1 Function receives the array, the target, the start position (first iteration will be 0 the beginnin), end position will be the last index
- * 2 Determine the mid position of the array: in this case 10 / 2 = 5
- * 3 if start position is more than end position ?? we finish 
- *  
- */
+//number of times repeated: 1, 2, 3, 4, 5
