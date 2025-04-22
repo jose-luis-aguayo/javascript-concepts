cdc2027d1a2b173f1f39ce542afb7114cb856ffd
Jose Luis Aguayo <jose-luis@MacBook-Pro-de-Jose.local>
2025-04-22 08:45:39 -0600

    binary tree

--- data-structures/binary-search-tree.js
+++ data-structures/binary-search-tree.js
@@ -26,7 +26,27 @@ class BST{
 
     insertNode(root, newNode){
         if(newNode.value < root.value){
-            
+            if(root.left == null){
+                root.left = newNode;
+            } else {
+                return this.insertNode(root.left, newNode);
+            }
+        } else {
+            if(root.right === null){
+                root.right = newNode;
+            } else {
+                return insertNode(root.right, newNode);
+            }
         }
     }
 }
+
+let bst = new BST();
+
+bst.insert(10);
+bst.insert(5);
+bst.insert(15);
+bst.insert(3);
+bst.insert(2);
+
+console.log(bst.root.left)
